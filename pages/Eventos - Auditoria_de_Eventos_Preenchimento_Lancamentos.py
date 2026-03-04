37596a9be7de9c626257082ad68b4985d9392d65
thais-fujissaka <thais.fujissaka@gmail.com>
2026-03-04 20:21:27 -0300

    feat: adiciona botao download farol auditoria eventos

--- pages/Eventos - Auditoria_de_Eventos_Preenchimento_Lancamentos.py
+++ pages/Eventos - Auditoria_de_Eventos_Preenchimento_Lancamentos.py
@@ -202,10 +202,17 @@ def main():
 			df_valor_total_categorias['Valor Total Categorias'] = df_valor_total_categorias[colunas_valor].sum(axis=1)
 			df_valor_total_categorias['Diferença'] = df_valor_total_categorias['Valor Total Evento'] - df_valor_total_categorias['Valor Total Categorias']
 			df_valor_total_categorias = df_valor_total_categorias[df_valor_total_categorias['Diferença'] != 0]
-			colunas = ['ID Evento', 'Nome Evento', 'ID Casa', 'Casa', 'Data Evento', 'Valor Total Evento', 'Valor Total Categorias', 'Diferença', 'Status'] + colunas_valor
-			df_valor_total_categorias = format_columns_brazilian(df_valor_total_categorias, ['Valor Total Evento', 'Valor Total Categorias', 'Diferença'] + colunas_valor)
+			colunas = ['ID Evento', 'Nome Evento', 'ID Casa', 'Casa', 'Data Evento', 'Valor Total Evento', 'Valor Total Categorias', 'Diferença', 'Status']
+			df_valor_total_categorias_download = df_valor_total_categorias[colunas].copy()
+			df_valor_total_categorias = format_columns_brazilian(df_valor_total_categorias, ['Valor Total Evento', 'Valor Total Categorias', 'Diferença'])
 			df_valor_total_categorias = df_format_date_columns_brazilian(df_valor_total_categorias, ['Data Evento'])
-			st.dataframe(df_valor_total_categorias[colunas], width='stretch', hide_index=True)
+			if df_valor_total_categorias.empty:
+				st.success("Nenhum evento com diferença de Valor Total Eventos e Valor Total de categorias.")
+			else:
+				st.dataframe(df_valor_total_categorias[colunas], width='stretch', hide_index=True)
+			col1, col2 = st.columns([4, 1])
+			with col2:
+				button_download(df_valor_total_categorias_download, f"valor_total_categorias", f"valor_total_categorias")
 
 	st.markdown("")
 if __name__ == '__main__':
