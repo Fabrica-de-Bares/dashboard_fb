5244b6bb51a34d20bccf9c4749bd03ef8b592cd9
thais-fujissaka <thais.fujissaka@gmail.com>
2026-03-10 12:01:19 -0300

    feat: atualiza comissao blue note

--- pages/Eventos - Acompanhamento_de_Comissão.py
+++ pages/Eventos - Acompanhamento_de_Comissão.py
@@ -263,7 +263,7 @@ def main():
                         df_recebimentos_gerente_priceless = calcular_comissao_gerente_priceless(df_recebimentos_total_mes_outros_vendedores, vendedor, id_casa)
                         df_vendedor = pd.concat([df_vendedor, df_recebimentos_gerente_priceless], ignore_index=True)
 
-                    if 110 in ids_casas_vendedor and cargo_vendedor == 'Gerente de Eventos':
+                    if 110 in ids_casas_vendedor:
                         df_recebimentos_total_mes_outros_vendedores = df_recebimentos_total_mes[df_recebimentos_total_mes['ID Casa'] == 110].copy()
                         df_recebimentos_gerente_blue_note = calcular_comissao_gerente_blue_note(df_recebimentos_total_mes, vendedor, id_casa)
                         df_vendedor = pd.concat([df_vendedor, df_recebimentos_gerente_blue_note], ignore_index=True)
