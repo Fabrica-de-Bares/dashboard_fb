55bc18571a855855156775bf57d90f77bd2a8001
cat-macedo <catarina.scabelli@gmail.com>
2026-04-23 11:05:25 -0300

    cria aba Headcount e formatação para inserção

--- utils/functions/general_functions.py
+++ utils/functions/general_functions.py
@@ -246,6 +246,8 @@ def config_sidebar():
         144: "Controladoria",
         141: "Planejamento Anual",
         142: "Planejamento Anual",
+        145: "Planejamento Anual",
+
     }
 
     cargo, user_name, email = config_permissoes_user()
@@ -279,7 +281,7 @@ def config_sidebar():
             "Fluxo de Caixa": [118, 120, 119],
             "Controladoria": [135, 136, 138, 139, 143, 144],
             "Auditoria": [133, 134],
-            "Planejamento Anual": [141, 142],
+            "Planejamento Anual": [141, 142, 145],
         }
 
         # Organizar abas por secao
