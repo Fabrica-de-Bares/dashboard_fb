2211305e3b1e0800cfac7a7ffea71777dc794ff0
cat-macedo <catarina.scabelli@gmail.com>
2026-06-25 16:59:47 -0300

    feat: implementa variação ano a ano

--- pages/Faturamento - Faturamento ZigPay - Média por dia da semana.py
+++ pages/Faturamento - Faturamento ZigPay - Média por dia da semana.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from utils.components import seletor_ano, input_multiselecao_casas, button_download
 from utils.functions.general_functions import config_sidebar
-from utils.functions.general_functions_conciliacao import formata_df
 from utils.queries_forecast import GET_ITENS_VENDIDOS_DIA_DA_SEMANA
 from utils.functions.faturamento_dia_semana import *
 from utils.functions.forecast import *
@@ -25,23 +24,24 @@ col1, col2 = st.columns([5, 1], vertical_alignment='center')
 with col1:
     st.title("💰 Faturamento ZigPay - Dias da Semana")
     st.markdown("""
-    - Para uma casa e ano selecionados, exibe a média de faturamento desse período incluindo Alimentos, Bebidas, Couvert, Gifts e Serviço por dia da semana
-    - Para meses futuros, calcula uma projeção baseada nas duas semanas anteriores
+    - Para uma casa selecionada, é exibida a média de faturamento (incluindo Alimentos, Bebidas, Couvert, Gifts e Serviço) por dia da semana de cada mês, do ano anterior e do atual, com cálculo da variação.
+    - Para meses futuros, é calculada uma projeção baseada nas duas semanas anteriores.
     """)
 with col2:
     st.button(label="Atualizar dados", on_click=st.cache_data.clear)
 st.divider()
 
 # Seletores
-col1, col2 = st.columns(2, vertical_alignment='center')
+# col1, col2 = st.columns(2, vertical_alignment='center')
 
-with col1:
-    lista_retirar_casas = ['Todas as Casas', 'Terraço Notie']
-    df_casas_selecionadas = input_multiselecao_casas(lista_retirar_casas, key='faturamento_bruto', adicionar_delivery=True)
-    lista_casas_selecionadas = df_casas_selecionadas['Casa'].tolist()
-    lista_ids_casas_selecionadas = df_casas_selecionadas['ID_Casa'].tolist()
-with col2: 
-    ano = seletor_ano(2025, 2026, 'ano_faturamento_zig', 'Selecione um ano:')
+# with col1:
+lista_retirar_casas = ['Todas as Casas', 'Terraço Notie']
+df_casas_selecionadas = input_multiselecao_casas(lista_retirar_casas, key='faturamento_bruto', adicionar_delivery=True)
+lista_casas_selecionadas = df_casas_selecionadas['Casa'].tolist()
+lista_ids_casas_selecionadas = df_casas_selecionadas['ID_Casa'].tolist()
+qtd_casas_selecionadas = len(lista_casas_selecionadas)
+# with col2: 
+#     ano = seletor_ano(2025, 2026, 'ano_faturamento_zig', 'Selecione um ano:')
 st.divider()
 
 if lista_casas_selecionadas == []:
@@ -55,56 +55,86 @@ df_faturamento_diario = GET_ITENS_VENDIDOS_DIA_DA_SEMANA()
 
 # Filtrando por casa e gerando coluna com dia da semana
 df_faturamento_diario_casa = prepara_dados_faturamento_casa(df_faturamento_diario, lista_casas_selecionadas)
+if df_faturamento_diario_casa.empty:
+    st.warning(f'Sem dados de faturamento para a(s) casa(s) selecionada(s).')
+    st.stop()
 
 # Gera projeção para prox dias do mês corrente/seguinte por dia da semana
-df_dias_futuros_com_categorias = lista_dias_mes_anterior_atual(
-    datas['ano_atual'],  
-    df_faturamento_diario_casa
-)
+df_dias_futuros_com_categorias = lista_dias_mes_anterior_atual(datas['ano_atual'], df_faturamento_diario_casa)
 
 df_dias_futuros_mes = cria_projecao_mes_corrente(df_faturamento_diario_casa, df_dias_futuros_com_categorias)
-df_dias_futuros_mes['Mes_Ano'] = df_dias_futuros_mes['Mes_Ano'].fillna(df_dias_futuros_mes['Data Evento'].dt.strftime('%m-%Y'))
+df_dias_futuros_mes['Mes_Ano'] = df_dias_futuros_mes['Mes_Ano'].fillna(df_dias_futuros_mes['Data Evento'].dt.strftime('%m/%Y'))
 
 # Une meses já concluídos com mês corrente
 if len(lista_casas_selecionadas) == 1:
     id_casa = lista_ids_casas_selecionadas[0]
     casa = lista_casas_selecionadas[0]
 else:
-    id_casa = 'Agrupamento'
-    casa = 'Agrupamento'
-df_faturamento_todos_meses = concatena_meses_reais_projetados(df_dias_futuros_mes, df_faturamento_diario_casa, id_casa, casa, ano)
+    id_casa = 'Casas agrupadas'
+    casa = 'Casas agrupadas'
+df_faturamento_todos_meses = concatena_meses_reais_projetados(df_dias_futuros_mes, df_faturamento_diario_casa)
 
 # Calcula faturamento geral por dia da semana para cada mês
-pivot_faturamento_geral = calcula_faturamento_medio(df_faturamento_todos_meses, ano)
+pivot_faturamento_geral = calcula_faturamento_medio(df_faturamento_todos_meses, qtd_casas_selecionadas)
 
-# Formata e estiliza exibição
-pivot_faturamento_geral_formatado = formata_df(pivot_faturamento_geral)
-pivot_faturamento_geral_formatado = pivot_faturamento_geral_formatado.rename(columns={'Mes_Ano':'Mês-Ano'})
-if ano == datas['ano_atual']:
-    df_estilizado = pivot_faturamento_geral_formatado.style.apply(destaca_mes_atual_seguintes, axis=1) # Pinta mês corrente (ainda não concluído)
-    legenda = f"""
-        <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
-            <div style="width: 15px; height: 15px; background-color: rgba(255,255,224); border: 1px solid #ccc; margin-right: 10px;"></div>
-            <span style="font-size: 14px">Média de faturamento projetado (não real). Mês ainda não está concluído.</span>
-        </div>
-        """
-elif ano > datas['ano_atual']:
-    df_estilizado = pivot_faturamento_geral_formatado
-    legenda = f"""
-        <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
-            <span style="font-size: 14px"><b>Observação:</b> Esses valores são a média de faturamento projetado (não real).</span>
-        </div>
-        """
-elif ano < datas['ano_atual']:
-    df_estilizado = pivot_faturamento_geral_formatado
-    legenda = ""
+# Calcula variação entre ano anterior e atual
+pivot_faturamento_geral = pivot_faturamento_geral.rename(columns={'Mes_Ano':'Mês/Ano'})
+pivot_faturamento_geral['Mês'] = pivot_faturamento_geral['Mês/Ano'].str.split("/").str[0].astype(int)
+pivot_faturamento_geral['Ano'] = pivot_faturamento_geral['Mês/Ano'].str.split("/").str[1].astype(int)
+pivot_faturamento_geral = pivot_faturamento_geral[['Mês', 'Ano', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']]
+pivot_faturamento_geral_variacao = calcula_variacao_ano_anterior(pivot_faturamento_geral)
+
+# Aplica estilos e formatação
+dias_semana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']
+mask = pivot_faturamento_geral_variacao['Ano'] == 'Variação'
+
+for col in dias_semana: # Concatena variação (diferença) e variação (porcentagem)
+    pivot_faturamento_geral_variacao.loc[mask, col] = (
+        pivot_faturamento_geral_variacao.loc[mask, col]
+        .map(lambda x: f"{x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+        + " ("
+        + pivot_faturamento_geral_variacao.loc[mask, f'{col}_pct']
+            .map(lambda x: f"{x:.1%}")
+        + ")"
+    )
+
+colunas_pct = [c for c in pivot_faturamento_geral_variacao.columns if c.endswith('_pct')]
+df_exibicao = pivot_faturamento_geral_variacao.drop(columns=colunas_pct)
+# height = (len(pivot_faturamento_geral_variacao) + 1) * 35
+
+for col in dias_semana: # Formatação numérica
+    df_exibicao.loc[df_exibicao['Ano'] != 'Variação', col] = (
+        df_exibicao.loc[df_exibicao['Ano'] != 'Variação', col]
+        .apply(formato_br)
+    )
+
+# Aplica estilos (cores)
+df_estilizado = df_exibicao.style.apply(lambda row: aplica_estilos(row, pivot_faturamento_geral_variacao), axis=1)
+
+# if ano == datas['ano_atual']:
+legenda = f"""
+    <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
+        <div style="width: 15px; height: 15px; background-color: rgba(255,255,224); border: 1px solid #ccc; margin-right: 10px;"></div>
+        <span style="font-size: 14px">Média de faturamento projetado (não real) para meses futuros do ano atual.</span>
+    </div>
+    """
+# elif ano > datas['ano_atual']:
+#     df_estilizado = pivot_faturamento_geral_formatado
+#     legenda = f"""
+#         <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
+#             <span style="font-size: 14px"><b>Observação:</b> Esses valores são a média de faturamento projetado (não real).</span>
+#         </div>
+#         """
+# elif ano < datas['ano_atual']:
+#     df_estilizado = pivot_faturamento_geral_formatado
+#     legenda = ""
 
 col1, col2 = st.columns([5, 1], vertical_alignment='center')
 with col1:
-    st.subheader(f"Média de faturamento geral por dia da semana - {casa} - {ano}")
+    st.subheader(f"Média de faturamento geral por dia da semana - {casa}")
 with col2:
     button_download(pivot_faturamento_geral, f"Faturamento Dia da Semana", f"Faturamento Dia da Semana")
-st.dataframe(df_estilizado, hide_index=True, height=458)
+st.dataframe(df_estilizado, hide_index=True)
 st.markdown(legenda, unsafe_allow_html=True) # Exibe legenda
 
 st.divider()
@@ -120,34 +150,62 @@ with col1:
     categoria_selecionada = st.selectbox("Selecione uma categoria:", categorias_faturamento)
 
 # Calcula a média de faturamento de cada categoria por dia da semana
-pivot_faturamento_categoria_dia_semana = calcula_faturamento_medio(df_faturamento_todos_meses, ano, detalhamento_categoria=True, categoria_selecionada=categoria_selecionada)
+pivot_faturamento_categoria_dia_semana = calcula_faturamento_medio(df_faturamento_todos_meses, qtd_casas_selecionadas, detalhamento_categoria=True, categoria_selecionada=categoria_selecionada)
 
 with col2:
     button_download(pivot_faturamento_categoria_dia_semana, f"Faturamento por Categoria", f"Faturamento por Categoria")
 
-# Formata e estiliza exibição
-pivot_faturamento_categoria_dia_semana_formatado = formata_df(pivot_faturamento_categoria_dia_semana)
-pivot_faturamento_categoria_dia_semana_formatado = pivot_faturamento_categoria_dia_semana_formatado.rename(columns={'Mes_Ano':'Mês-Ano'})
+# Calcula variação entre ano anterior e atual
+pivot_faturamento_categoria_dia_semana = pivot_faturamento_categoria_dia_semana.rename(columns={'Mes_Ano':'Mês/Ano'})
+pivot_faturamento_categoria_dia_semana['Mês'] = pivot_faturamento_categoria_dia_semana['Mês/Ano'].str.split("/").str[0].astype(int)
+pivot_faturamento_categoria_dia_semana['Ano'] = pivot_faturamento_categoria_dia_semana['Mês/Ano'].str.split("/").str[1].astype(int)
+pivot_faturamento_categoria_dia_semana = pivot_faturamento_categoria_dia_semana[['Mês', 'Ano', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']]
+pivot_faturamento_categoria_dia_semana = calcula_variacao_ano_anterior(pivot_faturamento_categoria_dia_semana)
 
-if ano == datas['ano_atual']:
-    df_estilizado = pivot_faturamento_categoria_dia_semana_formatado.style.apply(destaca_mes_atual_seguintes, axis=1) # Pinta mês corrente (ainda não concluído)
-    legenda = f"""
-        <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
-            <div style="width: 15px; height: 15px; background-color: rgba(255,255,224); border: 1px solid #ccc; margin-right: 10px;"></div>
-            <span style="font-size: 14px">Média de faturamento projetado (não real). Mês ainda não está concluído.</span>
-        </div>
-        """
-elif ano > datas['ano_atual']:
-    df_estilizado = pivot_faturamento_categoria_dia_semana_formatado
-    legenda = f"""
-        <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
-            <span style="font-size: 14px"><b>Observação:</b> Esses valores são a média de faturamento projetado (não real).</span>
-        </div>
-        """
-elif ano < datas['ano_atual']:
-    df_estilizado = pivot_faturamento_categoria_dia_semana_formatado
-    legenda = ""
+# Aplica estilos e formatação
+mask = pivot_faturamento_categoria_dia_semana['Ano'] == 'Variação'
 
-st.dataframe(df_estilizado, hide_index=True, height=458)
+for col in dias_semana: # Concatena variação (diferença) e variação (porcentagem)
+    pivot_faturamento_categoria_dia_semana.loc[mask, col] = (
+        pivot_faturamento_categoria_dia_semana.loc[mask, col]
+        .map(lambda x: f"{x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+        + " ("
+        + pivot_faturamento_categoria_dia_semana.loc[mask, f'{col}_pct']
+            .map(lambda x: f"{x:.1%}")
+        + ")"
+    )
+
+colunas_pct = [c for c in pivot_faturamento_categoria_dia_semana.columns if c.endswith('_pct')]
+df_exibicao = pivot_faturamento_categoria_dia_semana.drop(columns=colunas_pct)
+# height = (len(pivot_faturamento_categoria_dia_semana) + 1) * 35
+
+for col in dias_semana: # Formatação numérica
+    df_exibicao.loc[df_exibicao['Ano'] != 'Variação', col] = (
+        df_exibicao.loc[df_exibicao['Ano'] != 'Variação', col]
+        .apply(formato_br)
+    )
+
+# Aplica estilos (cores)
+df_estilizado = df_exibicao.style.apply(lambda row: aplica_estilos(row, pivot_faturamento_categoria_dia_semana), axis=1)
+
+# if ano == datas['ano_atual']:
+legenda = f"""
+    <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
+        <div style="width: 15px; height: 15px; background-color: rgba(255,255,224); border: 1px solid #ccc; margin-right: 10px;"></div>
+        <span style="font-size: 14px">Média de faturamento projetado (não real) para meses futuros do ano atual.</span>
+    </div>
+    """
+# elif ano > datas['ano_atual']:
+#     df_estilizado = pivot_faturamento_categoria_dia_semana_formatado
+#     legenda = f"""
+#         <div style="display: flex; align-items: center; padding:10px; border:1px solid #ccc; border-radius:8px";>
+#             <span style="font-size: 14px"><b>Observação:</b> Esses valores são a média de faturamento projetado (não real).</span>
+#         </div>
+#         """
+# elif ano < datas['ano_atual']:
+#     df_estilizado = pivot_faturamento_categoria_dia_semana_formatado
+#     legenda = ""
+
+st.dataframe(df_estilizado, hide_index=True)
 st.markdown(legenda, unsafe_allow_html=True) # Exibe legenda
 
--- utils/functions/faturamento_dia_semana.py
+++ utils/functions/faturamento_dia_semana.py
@@ -6,13 +6,6 @@ from utils.functions.general_functions_conciliacao import calcular_datas, traduz
 # Filtrando Datas
 datas = calcular_datas()
 
-# Destaca colunas do mês atual e seguintes (faturamento projetado)
-def destaca_mes_atual_seguintes(row):
-    mes_atual = datas['inicio_mes_atual'].strftime('%m-%Y')
-    if row['Mês-Ano'] >= mes_atual:
-        return ['background-color: rgba(255,255,224)'] * (len(row))
-    return [''] * (len(row))
-
 
 def prepara_dados_faturamento_casa(df_faturamento_diario, casas_selecionadas):
     # Filtra por casa e ano
@@ -35,7 +28,7 @@ def prepara_dados_faturamento_casa(df_faturamento_diario, casas_selecionadas):
     )
 
     # Cria mês numérico
-    df_faturamento_diario_casa['Mes_Ano'] = df_faturamento_diario_casa['Data Evento'].dt.strftime('%m-%Y')
+    df_faturamento_diario_casa['Mes_Ano'] = df_faturamento_diario_casa['Data Evento'].dt.strftime('%m/%Y')
     df_faturamento_diario_casa = df_faturamento_diario_casa[['Casa', 'Categoria', 'Data Evento', 'Valor Bruto', 'Dia Semana', 'Nome Mes', 'Mes_Ano']]
 
     # Arcos não abre de segunda-feira: zera segundas com faturamento de A&B para não impactar na projeção (vêm de Eventos)
@@ -48,12 +41,12 @@ def prepara_dados_faturamento_casa(df_faturamento_diario, casas_selecionadas):
     return df_faturamento_diario_casa
 
 
-def concatena_meses_reais_projetados(df_dias_futuros_mes, df_faturamento_diario_casa, id_casa, casa, ano):
+def concatena_meses_reais_projetados(df_dias_futuros_mes, df_faturamento_diario_casa):
     # Usa a projeção para o mês corrente (ainda não está finalizado) e para o próximo ano
-    if ano == datas['ano_atual']:
-        df_projecao_futuro = df_dias_futuros_mes[df_dias_futuros_mes['Data Evento'].dt.year == datas['ano_atual']]
-    else:
-        df_projecao_futuro = df_dias_futuros_mes
+    # if ano == datas['ano_atual']:
+    #     df_projecao_futuro = df_dias_futuros_mes[df_dias_futuros_mes['Data Evento'].dt.year == datas['ano_atual']].copy()
+    # else:
+    df_projecao_futuro = df_dias_futuros_mes # Quero utilizar o ano anterior e o atual 
     df_projecao_futuro = df_projecao_futuro[['Categoria', 'Data Evento', 'Valor Final', 'Dia Semana', 'Nome Mes', 'Mes_Ano']]
 
     df_merge = pd.merge(
@@ -70,19 +63,23 @@ def concatena_meses_reais_projetados(df_dias_futuros_mes, df_faturamento_diario_
         lambda x: traduz_semana_mes(x, 'mes')
     )
     # Cria mês numérico
-    df_merge['Mes_Ano'] = df_merge['Data Evento'].dt.strftime('%m-%Y')
+    df_merge['Mes_Ano'] = df_merge['Data Evento'].dt.strftime('%m/%Y')
 
     return df_merge
 
 
 # Calcula faturamento geral (junta todas as categorias da Zig) por dia da semana para cada mês
-def calcula_faturamento_medio(df_faturamento_todos_meses, ano, detalhamento_categoria=False, categoria_selecionada=None):
+def calcula_faturamento_medio(df_faturamento_todos_meses, qtd_casas_selecionadas, detalhamento_categoria=False, categoria_selecionada=None):
     # garante que é número
     df_faturamento_todos_meses['Valor Final'] = pd.to_numeric(df_faturamento_todos_meses['Valor Final'], errors='coerce')
    
     # Filtra pelo ano selecionado o df que tem todos os meses do ano atual e seguinte
-    df_faturamento_todos_meses = df_faturamento_todos_meses[df_faturamento_todos_meses['Data Evento'].dt.year == ano]
-   
+    # df_faturamento_todos_meses = df_faturamento_todos_meses[df_faturamento_todos_meses['Data Evento'].dt.year == ano].copy()
+    df_faturamento_todos_meses = df_faturamento_todos_meses[ # Ano anterior e atual para comparação
+        (df_faturamento_todos_meses['Data Evento'].dt.year == datas['ano_atual']) | 
+        (df_faturamento_todos_meses['Data Evento'].dt.year == datas['ano_atual'] - 1)
+    ].copy()
+
     # Calcula a média de faturamento de cada categoria por dia da semana
     df_faturamento_categoria_dia_semana = df_faturamento_todos_meses.groupby(['Categoria', 'Dia Semana', 'Mes_Ano'], dropna=False, as_index=False)[['Valor Final']].mean()
 
@@ -90,8 +87,11 @@ def calcula_faturamento_medio(df_faturamento_todos_meses, ano, detalhamento_cate
         # Soma de todas as categorias por dia da semana
         df_faturamento_dia_semana = df_faturamento_categoria_dia_semana.groupby(['Dia Semana', 'Mes_Ano'], as_index=False)[['Valor Final']].sum()
     else:
-        df_faturamento_categoria_dia_semana_filtrado = df_faturamento_categoria_dia_semana[df_faturamento_categoria_dia_semana['Categoria'] == categoria_selecionada]
+        df_faturamento_categoria_dia_semana_filtrado = df_faturamento_categoria_dia_semana[df_faturamento_categoria_dia_semana['Categoria'] == categoria_selecionada].copy()
         df_faturamento_dia_semana = df_faturamento_categoria_dia_semana_filtrado[['Dia Semana', 'Mes_Ano', 'Valor Final']]
+    
+    # Média - divide pela quantidade de casas selecionadas (não apenas soma)
+    df_faturamento_dia_semana['Valor Final'] = df_faturamento_dia_semana['Valor Final'] / qtd_casas_selecionadas
 
     pivot_faturamento_geral = df_faturamento_dia_semana.pivot(
         index='Mes_Ano',
@@ -102,3 +102,59 @@ def calcula_faturamento_medio(df_faturamento_todos_meses, ano, detalhamento_cate
     pivot_faturamento_geral = pivot_faturamento_geral[['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']]
     pivot_faturamento_geral = pivot_faturamento_geral.reset_index()  # transforma Mes_Ano em coluna
     return pivot_faturamento_geral
+
+
+def calcula_variacao_ano_anterior(pivot_faturamento):
+    linhas = []
+    dias_semana = [col for col in pivot_faturamento.columns if col not in ['Mês', 'Ano']]
+    for mes in sorted(pivot_faturamento['Mês'].unique()):
+        df_mes = pivot_faturamento[pivot_faturamento['Mês'] == mes].copy()
+        linha_ano_anterior = df_mes[df_mes['Ano'] == datas['ano_atual'] - 1]
+        linha_ano_atual = df_mes[df_mes['Ano'] == datas['ano_atual']]
+
+        if len(linha_ano_anterior) and len(linha_ano_atual):
+            variacao_valor = linha_ano_atual.iloc[0][dias_semana] - linha_ano_anterior.iloc[0][dias_semana]
+            variacao_pct = linha_ano_atual.iloc[0][dias_semana] / linha_ano_anterior.iloc[0][dias_semana] - 1
+
+            linha_var = variacao_valor.to_frame().T
+            for col in dias_semana:
+                linha_var[f'{col}_pct'] = variacao_pct[col]
+            linha_var['Mês'] = mes
+            linha_var['Ano'] = 'Variação'
+            linhas.append(linha_ano_anterior)
+            linhas.append(linha_ano_atual)
+            linhas.append(linha_var)
+
+    pivot_faturamento = pd.concat(linhas, ignore_index=True)
+    return pivot_faturamento
+
+
+def aplica_estilos(row, df):
+    mes_atual = datas['mes_atual']
+    ano_atual = datas['ano_atual']
+    estilos = [''] * len(row)
+
+    row_original = df.loc[row.name] # Linha completa do dataframe original (com as colunas _pct)
+
+    if row['Ano'] == 'Variação':
+        for i, col in enumerate(row.index):
+            if col in ['Mês', 'Ano']:
+                estilos[i] = 'font-weight: 600'
+            elif col in ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']:
+                pct_col = f'{col}_pct'
+                if pd.notna(row_original[pct_col]):
+                    if row_original[pct_col] > 0: estilos[i] = 'font-weight:600; color:#198754;' # Verde
+                    elif row_original[pct_col] < 0: estilos[i] = 'font-weight:600; color:#dc3545;' # Vermelho
+                    else: estilos[i] = 'font-weight:600'
+        return estilos
+
+    if row['Mês'] >= mes_atual and row['Ano'] == ano_atual:
+        return ['background-color: rgba(255,255,224)'] * len(row) # Amarelo para meses futuros
+
+    return estilos
+
+
+def formato_br(x):
+    if pd.isna(x):
+        return ""
+    return f"{x:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
